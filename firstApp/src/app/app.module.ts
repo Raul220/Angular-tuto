85be3b15c35c26dc5aba3e76237a624778dc4930
Raul220 <raul.borregomarques@gmail.com>
2022-03-17 21:23:37 +0100

    Inline Component

--- firstApp/src/app/app.module.ts
+++ firstApp/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { EmpleadosComponent } from './empleados/empleados.component';
 import { EmpleadoComponent } from './empleado/empleado.component';
+import { EjemploInlineComponent } from './ejemplo-inline/ejemplo-inline.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmpleadosComponent,
-    EmpleadoComponent
+    EmpleadoComponent,
+    EjemploInlineComponent
   ],
   imports: [
     BrowserModule
